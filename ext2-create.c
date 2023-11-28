8c1f55057076bf65d1754bb919a47c204f001b30
Abhinav Amanaganti <abhinava5@g.ucla.edu>
2023-11-27 18:40:45 -0800

    Update ext2-create.c

--- ext2-create.c
+++ ext2-create.c
@@ -202,21 +202,27 @@ void write_superblock(int fd) {
 	superblock.s_inodes_count = NUM_INODES;
 	superblock.s_blocks_count = NUM_BLOCKS;
 	superblock.s_r_blocks_count = 0;
+
 	superblock.s_free_blocks_count = NUM_FREE_BLOCKS;
 	superblock.s_free_inodes_count = NUM_FREE_INODES;
+	
 	superblock.s_first_data_block = SUPERBLOCK_BLOCKNO; /* First Data Block */
-	superblock.s_log_block_size = 10;					/* 1024 */
-	superblock.s_log_frag_size = 10;						/* 1024 */
+	superblock.s_log_block_size = 0;				
+	superblock.s_log_frag_size = 0;		
+
 	superblock.s_blocks_per_group = NUM_BLOCKS;
 	superblock.s_frags_per_group = NUM_BLOCKS;
 	superblock.s_inodes_per_group = NUM_INODES;	/* 1024 */
+
 	superblock.s_mtime = 0;				/* Mount time */
 	superblock.s_wtime = current_time;	/* Write time */
 	superblock.s_mnt_count         = 0; /* Number of times mounted so far */
 	superblock.s_max_mnt_count     = INT16_MAX; /* Make this unlimited */
+	
 	superblock.s_magic = EXT2_SUPER_MAGIC; /* ext2 Signature */
 	superblock.s_state             = 1; /* File system is clean */
 	superblock.s_errors            = 1; /* Ignore the error (continue on) */
+	
 	superblock.s_minor_rev_level   = 0; /* Leave this as 0 */
 	superblock.s_lastcheck = current_time; /* Last check time */
 	superblock.s_checkinterval     = 0; /* Force checks by making them every 1 second */
@@ -224,9 +230,6 @@ void write_superblock(int fd) {
 	superblock.s_rev_level         = 0; /* Leave this as 0 */
 	superblock.s_def_resuid        = 0; /* root */
 	superblock.s_def_resgid        = 0; /* root */
-	printf("s_blocks_per_group: %u\n", superblock.s_blocks_per_group);
-printf("s_frags_per_group: %u\n", superblock.s_frags_per_group);
-printf("s_inodes_per_group: %u\n", superblock.s_inodes_per_group);
 	/* You can leave everything below this line the same, delete this
 	   comment when you're done the lab */
 	superblock.s_uuid[0] = 0x5A;
